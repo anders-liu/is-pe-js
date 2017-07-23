06fcb44bbaf9757f6ca9b4d0553ebb113c96a34b
Anders Liu <anders.liu@gmail.com>
2017-07-23 01:20:44 -0700

    Implement core logics.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # is-pe-js
-A JavaScript/Node.js module to check whether a given buffer is in PE (Portable Executable) format, and to check some basic information, for example, is it exe or dll? Is it 32-bit or 64-bit? Is it managed (Microsoft .NET executable)?
+A JavaScript/Node.js module to check whether a given buffer is *potentially* in PE (Portable Executable) format, and to check some basic information, for example, is it exe or dll? Is it 32-bit or 64-bit? Is it managed (Microsoft .NET executable)?
+
+> **Note**, this lib only checks some key points against a PE file, and even one of the APIs returns `true`, it only means such file is *potentially* in PE format; there might be certain files that pass these key points check but is *not* a valid PE file.
+>
+> And, there're certain tricky exe file can be very very tiny, they may reduce file size by overlapping PE header and DOS header and pruning many unnecessary structures; through these files are 'runnable' exe files, the APIs return `false` against them.
 
 ## Build and Test
 
--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,144 @@
 export type BufferType = Uint8Array;
 
 export function isPE(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID) != 0);
 }
 
 export function isPE32(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_64) == IS_VALID);
 }
 
 export function isPE64(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_64) == (IS_VALID | IS_64));
 }
 
 export function isExe(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_DLL) == IS_VALID);
 }
 
 export function isExe32(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_DLL & IS_64) == IS_VALID);
 }
 
 export function isExe64(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_DLL & IS_64) == (IS_VALID | IS_64));
 }
 
 export function isDll(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_DLL) == (IS_VALID | IS_DLL));
 }
 
 export function isDll32(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_DLL & IS_64) == (IS_VALID | IS_DLL));
 }
 
 export function isDll64(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_DLL & IS_64) == (IS_VALID | IS_DLL | IS_64));
 }
 
 export function isManagedPE(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_MGD) == (IS_VALID | IS_MGD));
 }
 
 export function isManagedPE32(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_MGD & IS_64) == (IS_VALID | IS_MGD));
 }
 
 export function isManagedPE64(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_MGD & IS_64) == (IS_VALID | IS_MGD | IS_64));
 }
 
 export function isManagedExe(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_MGD & IS_DLL) == (IS_VALID | IS_MGD));
 }
 
 export function isManagedExe32(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_MGD & IS_DLL & IS_64) == (IS_VALID | IS_MGD));
 }
 
 export function isManagedExe64(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_MGD & IS_DLL & IS_64) == (IS_VALID | IS_MGD | IS_64));
 }
 
 export function isManagedDll(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_MGD & IS_DLL) == (IS_VALID | IS_MGD | IS_DLL));
 }
 
 export function isManagedDll32(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_MGD & IS_DLL & IS_64) == (IS_VALID | IS_MGD | IS_DLL));
 }
 
 export function isManagedDll64(buf: BufferType): boolean {
-    return true;
+    return ((attr(buf) & IS_VALID & IS_MGD & IS_DLL & IS_64) == (IS_VALID | IS_MGD | IS_DLL | IS_64));
 }
 
-const enum PEAttr {
-    INVALID = 0,
-    IS_VALID = 1,
-    IS_DLL = 2,
-    IS_64 = 4,
-    IS_MGD = 8,
-}
+const IS_VALID = 1;
+const IS_DLL = 2;
+const IS_64 = 4;
+const IS_MGD = 8;
 
-function getAttr(buf: BufferType): PEAttr {
+function attr(buf: BufferType): number {
+    let result = 0;
+
     // Basic checking.
     if (!buf || buf.byteLength < 0x40 /* PE signature @ 0x3C+4 */) {
-        return PEAttr.INVALID;
+        return 0;
     }
 
     // DOS signature.
     if (buf[0] != 0x4D || buf[1] != 0x5A) {
-        return PEAttr.INVALID;
+        return 0;
     }
 
     // PE signature.
-    const pe_ptr = buf[0x3C] | buf[0x3D] << 8 | buf[0x3E] << 16 | buf[0x3F] << 24;
-    if (buf.byteLength < pe_ptr + 4) {
-        return PEAttr.INVALID;
+    const peSigPtr = buf[0x3C] | (buf[0x3D] << 8) | (buf[0x3E] << 16) | (buf[0x3F] << 24);
+    if (buf.byteLength < peSigPtr + 4) {
+        return 0;
     }
 
-    if (buf[pe_ptr] != 0x50 || buf[pe_ptr + 1] != 0x45 || buf[pe_ptr + 2] != 0 || buf[pe_ptr + 3] != 0) {
-        return PEAttr.INVALID;
+    if (!(buf[peSigPtr] == 0x50 && buf[peSigPtr + 1] == 0x45 && buf[peSigPtr + 2] == 0 && buf[peSigPtr + 3] == 0)) {
+        return 0;
     }
 
-    return 0;
+    // Characteristics
+    const cPtr = peSigPtr + 22;
+    if (buf.byteLength < peSigPtr + 2) {
+        return 0;
+    }
+
+    const ch = buf[cPtr] | (buf[cPtr + 1] << 8);
+    if (ch & 0x2000) {
+        result |= IS_DLL;
+    }
+
+    // Magic number.
+    const magicPtr = peSigPtr + 24;
+    if (buf.byteLength < magicPtr + 2) {
+        return 0;
+    }
+
+    if (buf[magicPtr] == 0x0B && buf[magicPtr + 1] == 0x01) {
+        // 32-bit, nothing to set;
+    } else if (buf[magicPtr] == 0x0B && buf[magicPtr + 1] == 0x02) {
+        result |= IS_64;
+    } else {
+        return 0;
+    }
+
+    // Data directories.
+    const optSzPtr = peSigPtr + 20;
+    const optSz = buf[optSzPtr] | (buf[optSzPtr + 1] << 8);
+    const optEnd = magicPtr + optSz;
+
+    if (buf.byteLength < optEnd) {
+        return 0;
+    }
+
+    const ddComPtr = optEnd - 16;
+    if (buf[ddComPtr] != 0 || buf[ddComPtr + 1] != 0 || buf[ddComPtr + 2] != 0 || buf[ddComPtr + 3] != 0
+        || buf[ddComPtr + 4] != 0 || buf[ddComPtr + 5] != 0 || buf[ddComPtr + 6] != 0 || buf[ddComPtr + 7] != 0) {
+        result |= IS_MGD;
+    }
+
+    return result | IS_VALID;
 }
